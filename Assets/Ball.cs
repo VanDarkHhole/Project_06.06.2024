2e5e12b69e5ff0f67ae0775edcf7068b4ccfbb5f
VanDarkHolme <vbiletskyi@edu.cdv.pl>
2024-06-20 19:40:59 +0200

    Added speed uping when player hit the ball

--- Assets/Ball.cs
+++ Assets/Ball.cs
@@ -40,6 +40,15 @@ public class Ball : MonoBehaviour
         }
     }
 
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.GetComponent<Movement>() == null)
+            return;
+
+        collision.gameObject.GetComponent<Movement>().speed *= 1.1f;
+        rigidbody2D.velocity *= 1.1f;
+    }
+
 
 
     private void OnTriggerEnter2D (Collider2D collision)
